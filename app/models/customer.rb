ed682dce4d78c4c912033ca5714664855755dcdf
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2023-10-31 12:57:36 -0500

    add: model validations and relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,9 @@
 class Customer < ApplicationRecord
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+
+  has_many :subscriptions
+  has_many :teas, through: :subscriptions
 end
