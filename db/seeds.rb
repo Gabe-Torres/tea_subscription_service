f34bd72c30f75d76366ae45d95b9c499bd668e95
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2023-10-31 22:46:34 -0500

    add seeds for postman

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'factory_bot'
+include FactoryBot::Syntax::Methods
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+customers = create_list(:customer, 7)
+
+teas = create_list(:tea, 10)
+
+customers.each do |customer|
+  teas.each do |tea|
+    create(:subscription, customer: customer, tea: tea)
+  end
+end
+
