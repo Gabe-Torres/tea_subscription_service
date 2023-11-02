4e23f99279aeabcc15b994df7473020595b3b4c5
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2023-11-02 18:39:35 -0500

    refactor: fix error handling

--- spec/request/api/v0/update_subscription_spec.rb
+++ spec/request/api/v0/update_subscription_spec.rb
@@ -28,27 +28,20 @@ RSpec.describe "Update Subscription API", type: :request do
   end
   
   context "PATCH /subscriptions sad path" do
-    scenario "returns an error if subscription is not updated" do
+    scenario "returns an error if subscription doesnt exit" do
       customer1 = create(:customer)
       tea = create(:tea)
 
-      invalid_subscription_params = {
-        tea_id: 123,
-        customer_id: 123,
-        status: true,
-        price: 10.00,
-        frequency: "monthly",
-        title: "Special Tea"
-      }
-      
+      invalid_subscription_params = { status: false}
+
       header = { "CONTENT_TYPE" => "application/json" }
 
-      post "/api/v0/subscriptions", headers: header, params: invalid_subscription_params.to_json
+      patch "/api/v0/subscriptions/111", headers: header, params: invalid_subscription_params.to_json
 
       error_response = JSON.parse(response.body, symbolize_names: true)
       expect(response).to have_http_status(:bad_request)
       expect(response.status).to eq(400)
-      expect(error_response[:error]).to eq("Validation failed: Customer must exist, Tea must exist")
+      expect(error_response[:error]).to eq("Couldn't find Subscription with 'id'=111")
     end
   end
 end
